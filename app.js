c86b77d2f794ac80c5b6fc0ce99652f69237c824
Francesca3003 <160511339+Francesca3003@users.noreply.github.com>
2024-06-26 23:56:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+const sayHelllo = require("./module.js");
+sayHelllo("Francesca");
---
+++ module.js
@@ -0,0 +1,9 @@
+//Exercise 3 - Create and use CommonJS modules
+
+//Create a script that uses module.exports to export a function.
+//Create another script that uses require() to import the function and then calls it.
+
+function sayHello(name) {
+  console.log(`Hello, ${name}!`);
+}
+module.exports = sayHello;
